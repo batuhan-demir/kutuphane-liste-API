b2f26e6d604d47e66de300819cb28c2f2a515da7
Batuhan Demir <batuhandemir2005@gmail.com>
2023-11-12 21:15:53 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+
+const getList = async (il) => {
+
+    const file = await fs.readFileSync("./list.json", "utf-8");
+    const json = JSON.parse(file);
+    const body = json.find(el => el.il == il)?.body;
+
+    const req = await fetch("https://earsiv.gov.tr/kutuphane-listesi.aspx", {
+        "headers": {
+            "content-type": "application/x-www-form-urlencoded",
+        },
+        body,
+        "method": "POST"
+    });
+    const res = await req.text();
+
+    let sonuc = res.split("<tbody>")[1].split("</tbody>")[0];
+
+    sonuc = sonuc.split("'>")
+
+    sonuc = sonuc.map(el => el.split("</td>").map(a => a.trim().substring(4, a.length)));
+
+    sonuc.shift();
+
+    sonuc = sonuc.map(el => el = {
+        il: el[0],
+        ilce: el[1],
+        isim: el[2],
+        acik: el[3]?.split("</i> ")[1],
+        adres: el[4],
+        tel: el[5],
+        email: el[7],
+    })
+
+    return sonuc;
+}
+module.exports = {
+    getList
+}
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+require("dotenv").config();
+
+const app = express();
+
+const { getList } = require("./index");
+
+const PORT = process.env.PORT || 80;
+
+app.get("/", async (req, res) => {
+    const { il } = req.query;
+
+    if (!il)
+        return res.send("Bir il query'si girin.");
+
+    const sonuc = await getList(il);
+
+    res.json(sonuc);
+})
+
+
+app.listen(PORT, () => console.log(`${PORT} portu üzerinde dinlemeye başladım.`));
